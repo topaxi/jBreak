02a250a4f973d52a264cfe836ddea5d2884bb796
Damian Senn <ds@adfinis.com>
2009-12-29 17:37:38 +0100

    do not create audio tags if volume is 0

--- jBreak.js
+++ jBreak.js
@@ -89,8 +89,8 @@ var jBreak = {
 
 		$('#jBreak').append($lives);
 	},
-	playSound:function(soundFile){
-		if(typeof Audio === 'undefined') return;
+	playSound:function(soundFile){ // I'M LEAKING LOTS OF MEMORY!!
+		if(this._volume === 0 || typeof Audio === 'undefined') return;
 		var audio = new Audio(soundFile);
 		audio.volume = this._volume/100;
 		audio.play();
