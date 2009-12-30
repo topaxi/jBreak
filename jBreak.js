a98de2c0f5d9e88694f7f84887bb338ec08991f7
Damian Senn <ds@adfinis.com>
2009-12-30 14:14:13 +0100

    defined options outside the jBreak JSON object deklaration

--- jBreak.js
+++ jBreak.js
@@ -375,99 +375,7 @@ var jBreak = {
 	_levelID:0,
 	_mousePosition:null,
 	// objects
-	options:{
-		showOptions:function(){
-			this.$options = $('<div class="optionsContainer"/>');
-			this.$options.css('position', 'absolute'); // @todo remove this
-
-			var $draggableHandle = $('<p class="draggableHandle ui-widget-header">Settings</p>');
-			this.$options.prepend($draggableHandle);
-
-			this.$options.draggable({
-				containment:'#jBreakField',
-				handle:'.draggableHandle',
-				scroll:false
-			});
-
-			this.$optionTabs = $('<div class="options"/>');
-
-			this.$optionTabs.append('<ul style="font-size:12px"><li><a href="#tabs-1">Sound</a></li><li><a href="#tabs-2">Level</a></li><li><a href="#tabs-3">About</a></li></ul>');
-			this.$optionTabs.append(this.soundOptions());
-			this.$optionTabs.append('<div id="tabs-2" style="text-align:center;height:220px">-under construction-</div>');
-			this.$optionTabs.append('<div id="tabs-3" style="text-align:center;height:220px"><p>jBreak 0.1.7</p><p style="font-size:11px">Written by Damian Senn<br /><br />Graphics and Sounds<br />by <a href="http://www.helleresonnen.com/">Jan Neversil</a><br /><br />Music (coming soon)<br />by <a href="http://www.alphatronic.net/">Dani Whiler</a></p></div>');
-
-			var $startButton = $(
-				'<button class="ui-state-default ui-corner-all" style="cursor:pointer" id="jBreakStart">Start</button>'
-			);
-			$startButton.hover(function(){
-				$startButton.addClass('ui-state-hover');
-			},function(){
-				$startButton.removeClass('ui-state-hover');
-			});
-
-			this.$optionTabs.append(
-				$('<p class="ui-widget" style="margin-bottom:.5em;text-align:center"/>').append($startButton)
-			);
-
-			jBreak.$field.append(this.$options.append(this.$optionTabs));
-			this.$optionTabs.tabs();
-
-			$startButton.bind('click.jBreakCreatePaddles',function(e){
-				e.stopPropagation();
-				$('#jBreak .optionsContainer').fadeOut(750);
-				jBreak.loadLevel(jBreak._levelID);
-				$startButton.unbind('.jBreakCreatePaddles');
-			});
-			
-			this.$options.fadeIn('slow');
-		},
-		soundOptions:function(){
-			var $soundOptions = $('<div id="tabs-1" style="height:220px"/>');
-
-			var $soundVolumeControl = $('<div/>');
-			var $soundVolumeSlider = $('<div/>');
-			$soundVolumeControl.css('font-size','11px');
-			$soundVolumeSlider.css({width:'170px',marginBottom:'8px'});
-			$soundVolumeSlider.slider({
-				animate:true,
-				value:jBreak._volume,
-				range:'min',
-				min:0,
-				max:100,
-				slide:function(e, ui){
-					jBreak._volume = ui.value;
-					createCookie('soundVolume', ui.value, 7);
-					$('#soundVolume').text(ui.value+'%');
-					jBreak.playSound('sound/pling1s.ogg');
-				}
-			});
-			$soundVolumeControl.append($soundVolumeSlider);
-			$soundVolumeControl.prepend('<p style="margin:0">Sound volume: <span id="soundVolume" style="float:right">'+jBreak._volume+'%</span></p>');
-			$soundOptions.append($soundVolumeControl);
-
-			var $musicVolumeControl = $('<div/>');
-			var $musicVolumeSlider = $('<div/>');
-			$musicVolumeControl.css('font-size','11px');
-			$musicVolumeSlider.css('width','170px');
-			$musicVolumeSlider.slider({
-				animate:true,
-				value:70,
-				range:'min',
-				min:0,
-				max:100,
-				slide:function(e, ui){
-					//jBreak._volume = ui.value;
-					$('#musicVolume').text(ui.value+'%');
-				}
-			}).slider('disable');
-			$musicVolumeControl.append($musicVolumeSlider);
-			$musicVolumeControl.prepend('<p style="margin:0">Music volume: <span id="musicVolume" style="float:right">70%</span></p>');
-			$soundOptions.append($musicVolumeControl);
-			return $soundOptions;
-		},
-		$options:null,
-		$optionsTabs:null
-	},
+	options:null,
 	paddle:function(position){
 		// this references to the jBreak.paddle object!
 		this._init(position);
@@ -480,6 +388,100 @@ var jBreak = {
 		// this references to the jBreak.bonus object!
 		this._init(jBBall,x,y,angle);
 	}
+};
+
+jBreak.options = {
+	showOptions:function(){
+		this.$options = $('<div class="optionsContainer"/>');
+		this.$options.css('position', 'absolute'); // @todo remove this
+
+		var $draggableHandle = $('<p class="draggableHandle ui-widget-header">Settings</p>');
+		this.$options.prepend($draggableHandle);
+
+		this.$options.draggable({
+			containment:'#jBreakField',
+			handle:'.draggableHandle',
+			scroll:false
+		});
+
+		this.$optionTabs = $('<div class="options"/>');
+
+		this.$optionTabs.append('<ul style="font-size:12px"><li><a href="#tabs-1">Sound</a></li><li><a href="#tabs-2">Level</a></li><li><a href="#tabs-3">About</a></li></ul>');
+		this.$optionTabs.append(this.soundOptions());
+		this.$optionTabs.append('<div id="tabs-2" style="text-align:center;height:220px">-under construction-</div>');
+		this.$optionTabs.append('<div id="tabs-3" style="text-align:center;height:220px"><p>jBreak 0.1.7</p><p style="font-size:11px">Written by Damian Senn<br /><br />Graphics and Sounds<br />by <a href="http://www.helleresonnen.com/">Jan Neversil</a><br /><br />Music (coming soon)<br />by <a href="http://www.alphatronic.net/">Dani Whiler</a></p></div>');
+
+		var $startButton = $(
+			'<button class="ui-state-default ui-corner-all" style="cursor:pointer" id="jBreakStart">Start</button>'
+		);
+		$startButton.hover(function(){
+			$startButton.addClass('ui-state-hover');
+		},function(){
+			$startButton.removeClass('ui-state-hover');
+		});
+
+		this.$optionTabs.append(
+			$('<p class="ui-widget" style="margin-bottom:.5em;text-align:center"/>').append($startButton)
+		);
+
+		jBreak.$field.append(this.$options.append(this.$optionTabs));
+		this.$optionTabs.tabs();
+
+		$startButton.bind('click.jBreakCreatePaddles',function(e){
+			e.stopPropagation();
+			$('#jBreak .optionsContainer').fadeOut(750);
+			jBreak.loadLevel(jBreak._levelID);
+			$startButton.unbind('.jBreakCreatePaddles');
+		});
+		
+		this.$options.fadeIn('slow');
+	},
+	soundOptions:function(){
+		var $soundOptions = $('<div id="tabs-1" style="height:220px"/>');
+
+		var $soundVolumeControl = $('<div/>');
+		var $soundVolumeSlider = $('<div/>');
+		$soundVolumeControl.css('font-size','11px');
+		$soundVolumeSlider.css({width:'170px',marginBottom:'8px'});
+		$soundVolumeSlider.slider({
+			animate:true,
+			value:jBreak._volume,
+			range:'min',
+			min:0,
+			max:100,
+			slide:function(e, ui){
+				jBreak._volume = ui.value;
+				createCookie('soundVolume', ui.value, 7);
+				$('#soundVolume').text(ui.value+'%');
+				jBreak.playSound('sound/pling1s.ogg');
+			}
+		});
+		$soundVolumeControl.append($soundVolumeSlider);
+		$soundVolumeControl.prepend('<p style="margin:0">Sound volume: <span id="soundVolume" style="float:right">'+jBreak._volume+'%</span></p>');
+		$soundOptions.append($soundVolumeControl);
+
+		var $musicVolumeControl = $('<div/>');
+		var $musicVolumeSlider = $('<div/>');
+		$musicVolumeControl.css('font-size','11px');
+		$musicVolumeSlider.css('width','170px');
+		$musicVolumeSlider.slider({
+			animate:true,
+			value:70,
+			range:'min',
+			min:0,
+			max:100,
+			slide:function(e, ui){
+				//jBreak._volume = ui.value;
+				$('#musicVolume').text(ui.value+'%');
+			}
+		}).slider('disable');
+		$musicVolumeControl.append($musicVolumeSlider);
+		$musicVolumeControl.prepend('<p style="margin:0">Music volume: <span id="musicVolume" style="float:right">70%</span></p>');
+		$soundOptions.append($musicVolumeControl);
+		return $soundOptions;
+	},
+	$options:null,
+	$optionsTabs:null
 };
 
 jBreak.paddle.prototype = {
