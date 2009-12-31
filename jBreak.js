topaxi/jBreak

commit 004ef39af55700d9a3d5aea583ac7e9247062794

use sprite images for blocks

jBreak.js
@@ -348,25 +348,15 @@ var jBreak = {
 					var random = Math.ceil(Math.random()*10);
 					random = (random < 10 ? '0'+random : random);
 
-					/* @todo
-					 * create one a sprite image for each block theme to reduce
-					 * http requests
-					 */
-					var blockName = block.theme+'/'+random;
+					block.sprite = (random-1)*-16;
 					$block.css({
 						left:x*40,
 						top:y*16,
 						background:
 							'transparent url(images/blocks/'
-								+blockName+'.png) scroll no-repeat'});
-
-					// prefetch hit block image
-					var imageBlockCache = this._imageCache.blocks;
-					if(imageBlockCache[blockName] === undefined){
-						imageBlockCache[blockName] = $(
-							'<img src="images/blocks/'+blockName+'_h.png"/>'
-						);
-					}
+								+block.theme+'.png) scroll no-repeat',
+						backgroundPosition:'-40px '+block.sprite+'px'
+					});
 
 					this.$blocks.append($block);
 				}
@@ -862,8 +852,8 @@ jBreak.ball.prototype = {
 			               && jB.blocks[blockY][blockX] !== undefined;
 
 			if(blockExists){
-				var blockValue = jB.blocks[blockY][blockX].value;
-				if(blockValue > 0){
+				var block = jB.blocks[blockY][blockX];
+				if(block.value > 0){
 					jB.playSound('sound/pling1s.ogg');
 
 					if(!this._pierce){
@@ -897,29 +887,25 @@ jBreak.ball.prototype = {
 								(hHit && this._speed.x < 0 ? 'right' :
 									/*vHit && this._speed.y < 0*/ 'down')));
 
-					var hitImage = $block.css('background-image')
-						.replace(/\/(.*)\.png/g, '/$1_h.png');
-
 					var rand = Math.random();
-					if(blockValue > 1 && !this._pierce){
+					if(block.value > 1 && !this._pierce){
 						if(rand < .04)
 							new jB.bonus(this,x,y,180); // spawn bonus
 
-						var oldImage = $block.css('background-image');
 						$block.css({
 							opacity:1-1/blockValue,
-							backgroundImage:hitImage
+							backgroundPosition:'0 '+block.sprite+'px'
 						});
-						jB.blocks[blockY][blockX].value -= 1;
+						block.value -= 1;
 
 						setTimeout(function(){
-							$block.css('background-image', oldImage);
+							$block.css('background-position', '-40px'+block.sprite+'px');
 						}, 100);
 					} else {
 						if(rand < .08)
 							new jB.bonus(this,x,y,180); // spawn bonus
 
-						$block.css('background-image', hitImage);
+						$block.css('background-position', '0 '+block.sprite+'px');
 						$block.effect('drop', {direction:direction}, 'fast', function(){
 							$block.remove();
 						});

commit b3e347d836e0c7e454b91e3876d56fa39bc7a961

fix multihitblock regression from 004ef39af55700d9a3d5aea583ac7e9247062794

jBreak.js
@@ -915,7 +915,7 @@ jBreak.ball.prototype = {
 							new jB.bonus(this,x,y,180); // spawn bonus
 
 						$block.css({
-							opacity:1-1/blockValue,
+							opacity:1-1/block.value,
 							backgroundPosition:'0 '+block.sprite+'px'
 						});
 						block.value -= 1;