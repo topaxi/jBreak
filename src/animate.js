5caf4f3501aa28b0460ebc0add7f608f0f4777bf
Damian Senn <damian.senn@gmail.com>
2011-02-21 19:18:10 +0100

    int snap css and hitcheck

--- src/animate.js
+++ src/animate.js
@@ -2,14 +2,14 @@ function animate(){
 	var x = this._position.x + this._speed.x*4,
 	    y = this._position.y + this._speed.y*4;
 
-	this.move(x,y)._hitCheck(x,y);
+	this.move(x,y)._hitCheck(~~x, ~~y);
 
 	if(this._timer)
 		setTimeout(this._animate, this._interval);
 }
 
 function move(x,y){
-	this.$el.css({left:x, top:y});
+	this.$el.css({left: ~~x, top: ~~y});
 	this._position = {x:x,y:y};
 
 	return this;
